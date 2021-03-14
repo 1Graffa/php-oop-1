c8ccf1d1533bb4f9d2a6767ee204febbc4009da7
Giuseppe <giusepperaffa1988@gmail.com>
2021-03-15 00:03:10 +0100

    classe + proprietà

---
+++ oop/movie.php
@@ -0,0 +1,32 @@
+<?php
+// creo classe e proprietà
+class Movie{
+    public $paese;
+    public $genere;
+    public $durata;
+    public $regista;
+// dichiaro costruttore
+    public function __construct($_paese = null, $_genere = null){
+        $this->paese = $_paese;
+        $this->genere = $_genere;
+    }
+
+}
+// istanzio primo oggetto
+$italia = new Movie();
+// accedo all'oggetto instanziato
+$italia->paese = 'Film Italiano';
+$commedia->genere = "Commedia all'italiana";
+
+// istanzio secondo oggetto
+$usa = new Movie();
+$usa->paese = 'Film Americano';
+$horror->genere = 'Film Horror';
+
+// stampo a schermo
+echo $italia->paese;
+echo $commedia->genere;
+
+
+
+
